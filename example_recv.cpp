c1d14ea9feab7ea0cce5eb6c6e879706f199f80a
Andrew Stitcher <astitcher@apache.org>
2016-06-22 15:50:59 -0400

    Add in versions of the exampless actually used in the doc

---
+++ example_recv.cpp
@@ -0,0 +1,58 @@
+#include <proton/connection.hpp>
+#include <proton/default_container.hpp>
+#include <proton/delivery.hpp>
+#include <proton/messaging_handler.hpp>
+#include <proton/link.hpp>
+#include <proton/message_id.hpp>
+#include <proton/value.hpp>
+
+#include <iostream>
+#include <map>
+
+class simple_recv : public proton::messaging_handler {
+  private:
+    std::string url;
+    proton::receiver receiver;
+    uint64_t expected;
+    uint64_t received;
+
+  public:
+    simple_recv(const std::string &s, int c) : url(s), expected(c), received(0) {}
+
+    void on_container_start(proton::container &c) override {
+        receiver = c.open_receiver(url);
+        std::cout << "simple_recv listening on " << url << std::endl;
+    }
+
+    void on_message(proton::delivery &d, proton::message &msg) override {
+        if (proton::get<uint64_t>(msg.id()) < received) {
+            return; // Ignore duplicate
+        }
+
+        if (expected == 0 || received < expected) {
+            std::cout << msg.body() << std::endl;
+            received++;
+
+            if (received == expected) {
+                d.receiver().close();
+                d.connection().close();
+            }
+        }
+    }
+};
+
+int main(int argc, char **argv) {
+    std::string address("127.0.0.1:5672/examples");
+
+    int message_count = 100;
+    try {
+        simple_recv recv(address, message_count);
+        proton::default_container(recv).run();
+
+        return 0;
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 1;
+}
---
+++ example_send.cpp
@@ -0,0 +1,67 @@
+#include <proton/connection.hpp>
+#include <proton/default_container.hpp>
+#include <proton/messaging_handler.hpp>
+#include <proton/tracker.hpp>
+#include <proton/value.hpp>
+
+#include <iostream>
+#include <map>
+
+class simple_send : public proton::messaging_handler {
+  private:
+    std::string url;
+    proton::sender sender;
+    int sent;
+    int confirmed;
+    int total;
+
+  public:
+    simple_send(const std::string &s, int c) : url(s), sent(0), confirmed(0), total(c) {}
+
+    void on_container_start(proton::container &c) override {
+        sender = c.open_sender(url);
+    }
+
+    void on_sendable(proton::sender &s) override {
+        while (s.credit() && sent < total) {
+            proton::message msg;
+            std::map<std::string, int> m;
+            m["sequence"] = sent + 1;
+
+            msg.id(sent + 1);
+            msg.body(m);
+
+            s.send(msg);
+            sent++;
+        }
+    }
+
+    void on_tracker_accept(proton::tracker &t) override {
+        confirmed++;
+
+        if (confirmed == total) {
+            std::cout << "all messages confirmed" << std::endl;
+            t.connection().close();
+        }
+    }
+
+    void on_transport_close(proton::transport &) override {
+        sent = confirmed;
+    }
+};
+
+int main(int argc, char **argv) {
+    std::string address("127.0.0.1:5672/examples");
+    int message_count = 100;
+
+    try {
+        simple_send send(address, message_count);
+        proton::default_container(send).run();
+
+        return 0;
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 1;
+}
